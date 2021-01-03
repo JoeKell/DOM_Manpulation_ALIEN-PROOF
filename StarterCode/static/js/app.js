78f9511aa17cab13d587e6ab0b713ee527b09b13
GitJoeButt <jbkell2@yahoo.com>
2021-01-02 19:35:40 -0600

    Working on multiple filters. Isn't going to work yet.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,20 +12,26 @@ function runEnter() {
   
 
   // Get the value property of the input element
-  var inputValue = d3.select("#datetime").property("value");
-
-  console.log(inputValue);
+  var inputValueDate = d3.select("#datetime").property("value");
+  var inputValueCity = d3.select("#city").property("value");
+  var inputValueState = d3.select("#state").property("value");
+  var inputValueCountry = d3.select("#country").property("value");
+  var inputValueShape = d3.select("#shape").property("value");
 
   // Filter the data 
-  var filteredData = tableData.filter(value => value.datetime === inputValue);
+  var filteredData = tableData.filter(value => value.datetime === inputValueDate)
+  .filter(value => value.city === inputValueCity)  
+  .filter(value => value.state === inputValueState)
+  .filter(value => value.country === inputValueCountry)
+  .filter(value => value.shape === inputValueShape);
   
-  // This is to account for a blank input field
-  if (inputValue === ""){
-    var finalData = tableData;
-  }
-  else {
-    var finalData = filteredData;
-  }
+//   // This is to account for a blank input field
+//   if (inputValueData === "" ){
+//     var finalData = tableData;
+//   }
+//   else {
+//     var finalData = filteredData;
+//   }
 
   console.log(filteredData);
 
