3b439cf1efa863dcb08581f5b71b1998d2eb9318
GitJoeButt <jbkell2@yahoo.com>
2021-01-02 18:05:16 -0600

    The full table will show if the input is blank

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -17,22 +17,29 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputValue = d3.select("#datetime").property("value");
 
   console.log(inputValue);
 
+  // Filter the data 
   var filteredData = tableData.filter(value => value.datetime === inputValue);
+  
+  // This is to account for a blank input field
+  if (inputValue === ""){
+    var finalData = tableData;
+  }
+  else {
+    var finalData = filteredData;
+  }
 
   console.log(filteredData);
 
   // Add in the table data
   var tbody = d3.select("tbody");
   tbody.html("");
-  filteredData.forEach((sighting) => {
+  finalData.forEach((sighting) => {
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
       var cell = row.append("td");
